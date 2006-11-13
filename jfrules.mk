2c415dae8a40ee65410898e32d6b999f37afc927
Joachim Falk <falk@cs.fau.de>
2006-11-13 15:28:57 +0000

    * Added copyright boilerplate to all source files
    git-archimport-id: joachim.falk@gmx.de--2006-common-stuff/BuildSystem--mainline--0.2--patch-4

--- jfrules.mk
+++ jfrules.mk
@@ -1,4 +1,20 @@
 # vim: set noet sw=2 sts=0 ts=8:
+# Copyright (C) 2001 - 2006 Joachim Falk <joachim.falk@gmx.de>
+# 
+# This file is part of the BuildSystem distribution of Joachim Falk;
+# you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation;
+# either version 2 of the License, or (at your option) any later version.
+# 
+# The BuildSystem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public
+# License along with this program; If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
 
 pkgincludebacktrack=$(shell echo "$(pkgincludeprefix)" | sed -e 's@[^/][^/]*\(/\|$$\)@../@g')
 pkgincludedir=$(includedir)/$(pkgincludeprefix)
--- mkcompileheaderobj.sh
+++ mkcompileheaderobj.sh
@@ -1,5 +1,21 @@
 #! /bin/sh
 # vim: set sw=2 ts=8:
+# Copyright (C) 2001 - 2006 Joachim Falk <joachim.falk@gmx.de>
+# 
+# This file is part of the BuildSystem distribution of Joachim Falk;
+# you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation;
+# either version 2 of the License, or (at your option) any later version.
+# 
+# The BuildSystem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public
+# License along with this program; If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
 
 if [ $# -eq 0 ]; then
   echo "usage: `basename $0` headerfiles"
